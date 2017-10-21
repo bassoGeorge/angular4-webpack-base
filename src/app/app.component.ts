26f99478746e4709fd43da4fbdaa6df6a0d8d3fa
Anish George <anish.george@ignitesol.com>
2017-10-22 00:14:09 +0530

    Update typescript linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-    templateUrl: "./app.component.html",
-    styleUrls: ["./app.component.styl", "./app.component.css"]
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.styl', './app.component.css']
 })
 export class AppComponent { }
